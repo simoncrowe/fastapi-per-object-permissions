3ef8967ef9e8aef61cf42aa4464b7538e817c39a
Simon Crowe <simon.r.crowe@protonmail.com>
2022-12-11 00:58:46 +0000

    Add indexes

--- src/per_object_permissions/backends/postgres/queries/ensure_table_exists.sql
+++ src/per_object_permissions/backends/postgres/queries/ensure_table_exists.sql
@@ -3,3 +3,7 @@ CREATE TABLE IF NOT EXISTS perms (
 	predicate text,
 	object_uuid uuid
 );
+CREATE INDEX IF NOT EXISTS perms_subject_object_idx ON perms(subject_uuid, object_uuid);
+CREATE INDEX IF NOT EXISTS perms_subject_predicate_object_idx ON perms(
+	subject_uuid, predicate, object_uuid
+);
